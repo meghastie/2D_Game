7a5d42d42756ecdd97e85182825970f30fe63dc5
meghastie <meghastie@icloud.com>
2024-08-05 13:16:49 +0100

    Episode Four bug fixes

--- src/Main/GameWindow.java
+++ src/Main/GameWindow.java
@@ -6,10 +6,11 @@ public class GameWindow {
     private JFrame frame;
     public GameWindow(GamePanel gamePanel) {
         frame = new JFrame();
-        frame.setSize(400,400);
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE); //stop running program on close
         frame.add(gamePanel); //should now be able to see actual graphics
         frame.setLocationRelativeTo(null); //game window now in center
+        frame.setResizable(false); //dont allow window to be resized
+        frame.pack(); //tell jframe to set size of window to preferred size of components - we only have one, jpanel which has preferred size set in its methods
         frame.setVisible(true); //should be at end
     }
 }
