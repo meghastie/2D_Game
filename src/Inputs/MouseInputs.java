f2f4b923cdb553af28c4a7c83ba3d9e181c00102
meghastie <78167416+meghastie@users.noreply.github.com>
2024-08-05 12:51:29 +0100

    Revert "Episode Four"

--- src/Inputs/MouseInputs.java
+++ src/Inputs/MouseInputs.java
@@ -44,6 +44,7 @@ public class MouseInputs implements MouseListener, MouseMotionListener {
 
     @Override
     public void mouseMoved(MouseEvent e) {
-        gamePanel.setRecPos(e.getX(), e.getY());
+
+        //gamePanel.setRecPos(e.getX(), e.getY());
     }
 }
